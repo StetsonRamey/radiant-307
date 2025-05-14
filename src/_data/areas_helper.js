a245c016c2ad831620e3f049449b71dfb1a59ca1
StetsonRamey <me@stetson.dev>
2025-05-13 20:19:16 -0700

    Implement dynamic local markup and stretched link cards

--- src/_data/areas_helper.js
+++ src/_data/areas_helper.js
@@ -14,36 +14,37 @@ const areas = JSON.parse(areasData);
  * Flattens the nested areas data structure into an array of location objects
  * Each object contains the category, name, and slug
  */
-function getSlugs() {
-    const slugs = [];
+function getAreaData() {
+    const stuff = [];
 
     // Process each category (Cheyenne Neighborhoods, Nearby Cities)
     Object.entries(areas).forEach(([category, locations]) => {
         // Process each location within the category
         if (category === 'Cheyenne Neighborhoods') {
             Object.entries(locations).forEach(([name, data]) => {
-                slugs.push({
+                stuff.push({
                     slug: data.slug,
                     title: `${data.title} Neighborhood`,
                     description: data.description,
                     lat: data.location[0].lat,
-                    long: data.location[0].long
+                    long: data.location[0].long,
+                    localMarkup: data.localMarkup
                 });
             });
         } else {
             Object.entries(locations).forEach(([name, data]) => {
-                slugs.push({
+                stuff.push({
                     slug: data.slug,
                     title: `${data.title}, WY`,
                     lat: data.location[0].lat,
-                    long: data.location[0].long
+                    long: data.location[0].long,
                 });
             });
         }
     });
-    return slugs;
+    return stuff;
 }
 
 // Export the flattened locations directly
 // This will be available as a global data object in Eleventy
-export default getSlugs(); 
+export default getAreaData(); 
